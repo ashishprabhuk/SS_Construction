45bb857ded968e4bc77d06e5c1ad170b901a1a47
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2025-03-03 18:51:30 +0530

    UI Changes across the site

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -68,8 +68,8 @@ const ContactForm = () => {
               <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
             </svg>
           </div>
-          <h5 className="fw-bold text-uppercase small">FAX</h5>
-          <p className="small">1-234-567-8900</p>
+          <h5 className="fw-bold text-uppercase small">Socials</h5>
+          <p className="small">whatsapp</p>
         </Col>
 
         {/* Mail */}
