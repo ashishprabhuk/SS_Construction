1b27340331ed49ad16006bd288f26efa02b38c7d
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2025-03-03 19:48:40 +0530

    UI Changes across the site

--- src/Models/Slides.js
+++ src/Models/Slides.js
@@ -2,10 +2,14 @@ import banner1 from "../assets/Home/banner1.jpg";
 import banner2 from "../assets/Home/banner2.jpg";
 import banner3 from "../assets/Home/banner3.jpg";
 import banner4 from "../assets/Home/banner4.jpg";
+import banner5 from "../assets/Home/banner5.jpg";
+import banner6 from "../assets/Home/banner6.png";
 
 export const sliderImages = [
-  banner1,
   banner2,
   banner3,
-  banner4
+  banner4,
+  banner5,
+  banner6,
+  banner1,
 ];
